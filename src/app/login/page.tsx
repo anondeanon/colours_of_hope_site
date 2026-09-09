870e5f11573a7780a0928a060ea9b2b08c4e1643
anondeanon <dimka37773777@Gmail.com>
2026-09-09 03:09:24 +0300

    Final script

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,7 +18,6 @@ const handleSubmit = async (e: React.FormEvent) => {
     setIsLoading(true);
 
     try {
-      // ТУТ ВИПРАВЛЕНО: додано const response = await
       const response = await fetch('http://144.31.25.196:8000/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
